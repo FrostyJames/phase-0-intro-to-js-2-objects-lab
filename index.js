4c3b94eaa3f7fc5c5423a3896acfba203cfba2b6
james <ivanlavan773@gmail.com>
2025-06-06 23:51:57 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+// Initial employee object
+let employee = {
+    name: 'Sam'
+};
+
+// Non-destructive function to update employee
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+
+// Destructive function to update employee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+// Non-destructive function to delete a key from employee
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+// Destructive function to delete a key from employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}

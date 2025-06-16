1de534983314d2a672c36270399c0336fc0504bf
james <ivanlavan773@gmail.com>
2025-06-16 15:17:39 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-// Write your solution in this file!
-// Initial employee object
-let employee = {
+const employee = {
     name: 'Sam'
 };
 
-// Non-destructive function to update employee
+// Non-destructive update function
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return { ...employee, [key]: value };
+    return {
+        ...employee,
+        [key]: value
+    };
 }
 
-// Destructive function to update employee
+// Destructive update function
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
 }
 
-// Non-destructive function to delete a key from employee
+// Non-destructive delete function
 function deleteFromEmployeeByKey(employee, key) {
-    let newEmployee = { ...employee };
+    const newEmployee = { ...employee };
     delete newEmployee[key];
     return newEmployee;
 }
 
-// Destructive function to delete a key from employee
+// Destructive delete function
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
 }
+
+module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey
+};
